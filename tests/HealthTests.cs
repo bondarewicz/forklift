a94339e76c22e1fb0447c49c3fe1fef736235814
Łukasz Bondarewicz <lukasz@parcelvision.com>
2026-01-29 18:13:01 +0100

    test: Add health tests

---
+++ tests/HealthTests.cs
@@ -0,0 +1,36 @@
+using Aspire.Hosting;
+using Aspire.Hosting.ApplicationModel;
+using Aspire.Hosting.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Forklift.Tests;
+
+public class HealthTests
+{
+    [Fact]
+    public async Task AllServicesStartAndBecomeHealthy()
+    {
+        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.Forklift>();
+
+        await using var app = await appHost.BuildAsync();
+        await app.StartAsync();
+
+        var resourceNotificationService =
+            app.Services.GetRequiredService<ResourceNotificationService>();
+
+        using var cts = new CancellationTokenSource(TimeSpan.FromMinutes(5));
+
+        // Wait for all resources to be running
+        var resources = new[] { "postgres", "mongo", "redis", "rabbitmq", "eventstore", "unleash" };
+
+        foreach (var resource in resources)
+        {
+            await resourceNotificationService.WaitForResourceAsync(
+                resource,
+                KnownResourceStates.Running,
+                cts.Token
+            );
+        }
+    }
+}
